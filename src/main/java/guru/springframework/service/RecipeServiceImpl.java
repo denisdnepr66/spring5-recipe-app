7324d123fc5ff29c6ba2c925a2329500a2474e77
denisdnepr66 <56921743+denisdnepr66@users.noreply.github.com>
2021-04-11 13:01:27 +0300

    adding some lombok features

--- src/main/java/guru/springframework/service/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/service/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package guru.springframework.service;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,9 +20,9 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I am in the service");
 
         Set<Recipe> recipeSet = new HashSet<>();
-
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
     }
